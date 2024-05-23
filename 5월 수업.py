8fc269b76683d195468aa50cd8b1c5687a1b0d2c
ParkSeolRin <164719957+ParkSeolRin@users.noreply.github.com>
2024-05-23 15:04:14 +0900

    Add files via upload

--- 5월 수업.py
+++ 5월 수업.py
@@ -283,3 +283,164 @@
 # for a, b in zip(a_list, b_list):
 #     print(solution(a, b))
 
+# 05/23 =====================================================================================================================================
+
+# 11.정수 n, m이 주어졌을 때 가로 n X 세로 m 의 형태로 직사각형 모양의 '*' 출력
+# # Solution-1
+# def solution(n, m):
+#     for _ in range(m):
+#         for _ in range(n):
+#             print('*', end = '')
+#         print('')
+#
+# solution(5,3)
+
+# # Solution-2.
+# def solution(n, m):
+#     for _ in range(m):
+#         print('*' * n)
+#
+# solution(5,3)
+
+# 12. 문자열 s를 입력받아 가운데 글자를 출력 s의 길이가 홀수면 가운데 글자 하나만 반환, 짝수면 가운데 글자 2개를 반환
+
+# # Solution-1
+# def solution(s):
+#     if len(s) % 2 != 0:
+#         return s[len(s) // 2]
+#     else:
+#         return s[len(s) // 2 - 1] + s[len(s) // 2]
+#
+#
+# s = 'abcde' # 'c'    len(s) // 2
+# print(solution(s))
+# s = 'qwer' # 'we'    len(s) -> 4      len(s) // 2 - 1, len(s) // 2  => 2
+# print(solution(s))
+
+# 13. 두 행렬의 합
+# def solution(arr1, arr2):
+#     y, x = len(arr1), len(arr1[0])
+#     result = []
+#     for _ in range(y):
+#         row = [0] * x
+#         result.append(row)
+#
+#     for row in range(y):
+#         for col in range(x):
+#             result[row][col] = arr1[row][col] + arr2[row][col]
+#     return result
+
+# 14. 내적
+# a와 b의 내적 -> 컨블루션 연산
+
+# # Solution-1
+# def solution(a, b):
+#     result = 0
+#     for idx in range(len(a)):
+#         result += a[idx] * b[idx]
+#     return result
+
+# a = [1, 2, 3, 4]
+# b = [-3, -1, 0, 2]
+# print(solution(a, b))
+#
+# a = [-1, 0, 1]
+# b = [1, 0, -1]
+# print(solution(a, b))
+
+
+# # Solution-2
+# def solution(a, b):
+#     result = 0
+#     for idx in range(len(a)):
+#         result += a[idx] * b[idx]
+#
+#     result = sum(a[idx] * b[idx] for idx in range(len(a)))
+#     return result
+#
+# a = [1, 2, 3, 4]
+# b = [-3, -1, 0, 2]
+# print(solution(a, b))
+#
+# a = [-1, 0, 1]
+# b = [1, 0, -1]
+# print(solution(a, b))
+
+# 15. 문자열 s의 길이가 4 혹은 6이고, 숫자로만 구성돼있는지 확인
+# def solution(s):
+# # Solution-1
+    # if len(s) == 4 or len(s) == 6:
+    #     return s.isdigit()  # 문자열 s가 숫자인가?
+    # else:
+    #     return False
+
+
+# # Solution-2
+#     return True if (len(s) == 4 or len(s) == 6) and s.isdigit() else False
+#
+# s = 'a234'
+# print(solution(s))
+# s = '1234'
+# print(solution(s))
+# s = '12345'
+# print(solution(s))
+
+# 16. 연속적으로 나타나는 같은 숫자 제거
+
+# arr = [1,1,3,3,0,1,1]
+# 기준 arr[0] ==> 1
+
+# arr[0]과 arr[1]을 비교 ==> 1, 1 ==> 같은 숫자
+# arr[1] 삭제
+
+# 기준 arr[0] ==> 1
+# arr[0]과 arr[1]을 비교 ==> 1, 3 ==> 다른 숫자
+
+# len(arr) - 1 까지 While문으로 반복
+# ====================================================
+# arr = [1,1,3,3,0,1,1]
+# for문을 이용해서 len(arr) - 1까지 순회
+# visited => 마지막으로 관찰된 숫자
+# 현재 관찰중인 숫자 != visited ? 다르면 result 리스트에 추가
+
+# def solution(arr):
+#     visited = arr[0]
+#     result = []
+#     for idx in range(1, len(arr)):
+#         if arr[idx] != visited:
+#             result.append(visited)
+#         visited = arr[idx]
+#
+#     if result[-1] != visited:
+#         result.append(visited)
+#     return result
+#
+#
+# arr = [1,1,3,3,0,1,1]
+# print(solution(arr))
+# arr = [4,4,4,3,3]
+# print(solution(arr))
+
+
+# 17.
+# 각 단어의 짝수번째 인덱스 문자를 대문자
+#         홀수번째 인덱스 문자를 소문자
+
+# def solution(s):
+#     # 문자열.split(구분자)  => 구분자를 기준으로 문자열을 나눈다
+#     words = s.split(' ')
+#     for w_idx in range(len(words)):
+#         word = ''
+#         for c_idx in range(len(words[w_idx])):
+#             if c_idx % 2 == 0:
+#                 word += words[w_idx][c_idx].upper()
+#             else:
+#                 word += words[w_idx][c_idx].lower()
+#         words[w_idx] = word
+#
+#     result = ' '.join(words)
+#     return result
+#
+# s = "try hello world"
+# print(solution(s))
+
